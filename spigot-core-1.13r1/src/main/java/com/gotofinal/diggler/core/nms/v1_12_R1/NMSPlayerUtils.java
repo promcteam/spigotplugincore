945415fc988cd1b04846f3c2bf5d92b0cc14e8da
Marcin Wieczorek <marcin@marcin.co>
2018-07-23 10:35:10 +0200

    1.13 Update

---
+++ spigot-core-1.13r1/src/main/java/com/gotofinal/diggler/core/nms/v1_12_R1/NMSPlayerUtils.java
@@ -0,0 +1,35 @@
+package com.gotofinal.diggler.core.nms.v1_13_R1;
+
+import com.gotofinal.diggler.core.nms.ChatPosition;
+import com.gotofinal.diggler.core.nms.INMSPlayerUtils;
+
+import net.minecraft.server.v1_13_R1.ChatMessageType;
+import org.bukkit.craftbukkit.v1_13_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_13_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.HoverEvent.Action;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.minecraft.server.v1_13_R1.NBTTagCompound;
+import net.minecraft.server.v1_13_R1.PacketPlayOutChat;
+
+public class NMSPlayerUtils implements INMSPlayerUtils
+{
+	@Override
+	public void sendMessage(BaseComponent[] msg, ChatPosition chatPosition, Player player)
+	{
+		CraftPlayer p = (CraftPlayer) player;
+		PacketPlayOutChat packet = new PacketPlayOutChat(null, ChatMessageType.a((byte) chatPosition.ordinal()));
+		packet.components = msg;
+		p.getHandle().playerConnection.sendPacket(packet);
+	}
+
+	@Override
+	public HoverEvent convert(ItemStack itemStack)
+	{
+		return new HoverEvent(Action.SHOW_ITEM, new BaseComponent[]{new TextComponent(CraftItemStack.asNMSCopy(itemStack).save(new NBTTagCompound()).toString())});
+	}
+}
